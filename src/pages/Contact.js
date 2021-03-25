54813e21cda0f9bc83ce64d08561c1b302aec8cf
Kristabell Johnson <bell77iam@gmail.com>
2021-03-24 23:26:15 -0600

    contact.js localhost change to 5000

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -43,7 +43,7 @@ class Contact extends React.Component {
             disabled: true
         });
 
-        Axios.post('http://localhost:3000/api/email', this.state)
+        Axios.post('http://localhost:5000/api/email', this.state)
             .then(res => {
                 if (res.data.success) {
                     this.setState({
